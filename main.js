c3c66e9038da590a7f2c985184e832591d8810f5
lwl666666 <905375993@qq.com>
2017-08-08 17:29:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 $(function(){
 	debugger;
-	var side_bar_button = $("#side_bar_button"),
+	var side_bar_button = $(".logo"),
 		side_bar = $("#side_bar"),
 		mask = $("#mask");
+		back_to_top = $("#back_to_top");
 	side_bar_button.on("click",function(){
 		debugger;
-		$("#mask").fadeIn();
-		side_bar.animate({right:"0px"});
+		mask.fadeIn();
+		side_bar.css('left',0);
 	});
-	$("#side_bar a").on("click",function(){
-debugger;
-		$("#mask").fadeOut();
-		side_bar.animate({right:"-300px"});
-	})
+	mask.on("click",function(){
+		debugger;
+		mask.fadeOut();
+		side_bar.css('left',-side_bar.width());
+	});
+	back_to_top.on("click",function(){
+		$("html,body").animate({
+			'scrollTop':0
+		},1000);
+	});
+	$(window).on("scroll",function(){
+		if($(window).scrollTop()>$(window).height()){
+			back_to_top.fadeIn();
+		}else{
+			back_to_top.fadeOut();
+		}
+	});
+	$(".hr").animate({
+		"width":"300px"
+	},1500);
+	$(window).trigger("scroll");
 
 });
